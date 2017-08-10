f4e1e9f57809e7ee0c52bd580f3e193065929f31
Nicholas Arnaud <arnaudnicholas@gmail.com>
2017-08-10 09:22:42 -0500

    Hello Triangle

--- OpenGL Practice/Source.cpp
+++ OpenGL Practice/Source.cpp
@@ -11,8 +11,9 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 
+
 const char *vertexShaderSource = "#version 330 core\n"
-"layout(location = 0) in vec3 aPos;\n"
+"layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
@@ -72,6 +73,8 @@ int main()
 	}
 
 	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success)
@@ -90,8 +93,11 @@ int main()
 	};
 
 
-	unsigned int VBO;
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
@@ -99,14 +105,13 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(0);
+
 
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
+
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
@@ -118,8 +123,8 @@ int main()
 		glfwPollEvents();
 	}
 
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1,&VBO);
 
 	glfwTerminate();
 	return 0;
