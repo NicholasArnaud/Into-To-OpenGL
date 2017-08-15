b2970549c2dc60e54525c93eea697f3d519419b5
Nicholas Arnaud <arnaudnicholas@gmail.com>
2017-08-15 12:48:36 -0500

    Continued on Shader class

--- OpenGL Practice/Shader.h
+++ OpenGL Practice/Shader.h
@@ -12,11 +12,13 @@ class Shader
 public:
 	unsigned int ID;
 
-	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath, const char* geometryPath);
 	void use();
 	void setBool(const std::string &name, bool value) const;
 	void setInt(const std::string &name, int value) const;
 	void setFloat(const std::string &name, float value) const;
+private:
+	void checkCompileErrors(unsigned int shader, std::string type);
 };
 
 #endif
