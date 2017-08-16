8ee95e7f217cc4f4b052f85af3bcbb7a4a5462e7
Nicholas Arnaud <arnaudnicholas@gmail.com>
2017-08-16 12:56:13 -0500

    More into Textures

--- OpenGL Practice/Source.cpp
+++ OpenGL Practice/Source.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include "Shader.h"
 #include "FileReader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -22,23 +24,31 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-	
+
 	shaderFile.writeIt("VertShader.sr", "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
 		"layout (location = 1) in vec3 aColor;\n"
+		"layout (location = 2) in vec2 aTexCoord;\n"
+
 		"out vec3 ourColor;\n"
+		"out vec2 TexCoord;\n"
+
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos,1.0);\n"
 		"	ourColor = aColor;\n"
+		"	TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
 		"}\n\0");
 
 	shaderFile.writeIt("FragShader.sr", "#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"in vec3 ourColor; \n"
+		"in vec2 TexCoord; \n"
+		"uniform sampler2D texture1;\n"
 		"void main()\n"
 		"{\n"
 		"   FragColor = vec4(ourColor, 1.0f);\n"
+		"	//FragColor = texture(texture1, TexCoord);\n"
 		"}\n\0");
 
 
@@ -60,17 +70,12 @@ int main()
 	Shader ourShader("VertShader.sr", "FragShader.sr", nullptr);
 	float vertices[] =
 	{
-		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  
-		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  
-		0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+		// positions          // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f, //  1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // 1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  // 0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  // 0.0f, 1.0f    // top left 
 	};
-
-	float texCoords[] = {
-		0.0f, 0.0f,
-		1.0f, 0.0f,
-		0.5f, 1.0f
-	};
-
 
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
@@ -85,7 +90,30 @@ int main()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	//glEnableVertexAttribArray(2);
 
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load and generate the texture
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 
 	while (!glfwWindowShouldClose(window))
 	{
